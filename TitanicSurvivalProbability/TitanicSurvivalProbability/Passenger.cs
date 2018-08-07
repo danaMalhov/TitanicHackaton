49862830d0fd91c7126b1ee95c9e32c16b81b443
danaMalhov <42171553+danaMalhov@users.noreply.github.com>
2018-08-07 14:56:21 +0300

    Added properties to passenger

--- TitanicSurvivalProbability/TitanicSurvivalProbability/Passenger.cs
+++ TitanicSurvivalProbability/TitanicSurvivalProbability/Passenger.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace TitanicSurvivalProbability
 {
-    class Passenger
+    public enum SocialClass
     {
+        First,
+        Second,
+        Third
+    }
+
+    public enum Gender
+    {
+        female,
+        male
+    }
+
+    public class Passenger
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public SocialClass Class { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public double Fair { get; set; }
+        public int SurvivalProbability { get; set; }
     }
 }
